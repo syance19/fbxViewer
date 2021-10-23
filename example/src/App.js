df3713a300194d9fd90a8023cf7ab68b13b57f3f
syance19 <yance3@hotmail.com>
2021-10-22 22:40:51 -0500

    fbx

---
+++ example/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import ReactThreeFbxViewer from 'react-three-fbx-viewer';
+
+let fbxUrl = require('./asd.fbx');
+
+let fbx2= ('./01_Shoe.fbx')
+
+export default class App extends Component {
+	onLoad(e) {
+		console.log(e);
+	}
+
+	onError(e) {
+		console.log(e);
+	}
+	render () {
+		let cameraPosition = {
+			x:150,
+			y:300,
+			z:350
+		}
+
+    console.log(fbxUrl);
+		return (
+			<div>
+				<ReactThreeFbxViewer cameraPosition={cameraPosition} url={fbxUrl} onLoading={this.onLoad} onError={this.onError}/>
+
+			</div>
+		);
+	}
+}
